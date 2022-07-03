8dd00d393bf231f1f6ece21edf0b36356374b4ff
Natan de Paula <batistanatan010@gmail.com>
2022-07-03 10:36:22 -0300

    Implementação do formulario de inscrição

--- codBasico/controlerInscricao.php
+++ codBasico/controlerInscricao.php
@@ -1,4 +1,6 @@
 <?php
+    session_start();
+
     $categoria=[];
     $categoria[] = 'infantil';
     $categoria[] = 'adolescente';
@@ -10,13 +12,50 @@
     $idade =$_POST['lIdade'];
    //var_dump($nome);// retorna o tipo da variavel
 
-    if($idade >=0 && $idade <=12){
-        echo "O nadador ".$nome." esta inscrito na categoria ".$categoria[0];
-    }else if($idade >12 && $idade <=18){
-        echo "O nadador ".$nome." esta inscrito na categoria ".$categoria[1];
-    }else{
-        echo "O nadador ".$nome." esta inscrito na categoria ".$categoria[2];
-    }
+   $_SESSION['mensagem-de-erro']='';
+   $_SESSION['mensagem-de-erro-idade']='';
+   if(empty($nome)){
+       $_SESSION['mensagem-de-erro']=  '<b>O campo nome não deve ser vazio</b>';
+       if(!is_numeric($idade)) {
+            $_SESSION['mensagem-de-erro-idade']= '<b>Favor inserir uma idade valida</b>';
+       }
+       header('location:index.php');
+       return;
+   
+   }else if(strlen($nome) < 3) {
+        $_SESSION['mensagem-de-erro']= '<b>O nome deve ser composto por no minimo 3 letras</b>';
+        if(!is_numeric($idade)) {
+            $_SESSION['mensagem-de-erro-idade']= '<b>Favor inserir uma idade valida</b>';
+       }
+        header('location:index.php');
+        return;
 
+    }else if(strlen($nome) > 40) {
+        $_SESSION['mensagem-de-erro']= '<b>O nome não deve conter mais de 40 letras</b>';
+        if(!is_numeric($idade)) {
+            $_SESSION['mensagem-de-erro-idade']= '<b>Favor inserir uma idade valida</b>';
+       }
+        header('location:index.php');
+        return;
+
+    }
+    
+    if(is_numeric($idade)) {
+        if($idade >=0 && $idade <=12){
+            echo "O nadador ".$nome." esta inscrito na categoria ".$categoria[0];
+        }else if($idade >12 && $idade <=18){
+            echo "O nadador ".$nome." esta inscrito na categoria ".$categoria[1];
+        }else{
+            echo "O nadador ".$nome." esta inscrito na categoria ".$categoria[2];
+        }
+   }else{
+        $_SESSION['mensagem-de-erro-idade']= '<b>Favor inserir uma idade valida</b>';
+        header('location:index.php');
+        return;
+   }
+
+
+
+    
 
 ?>
--- codBasico/index.php
+++ codBasico/index.php
@@ -1,13 +1,22 @@
+<?php
+    session_start();// inicia a seção php
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8"> 
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formalario de inscrição de competidores</title>
     <style>
         #formulario{
             font-size: 25px;
+        }
+        .mensErro{
+            font-size: 18px;
+            color:#ff4d4d;
+            padding: top 0;;
+            
         }
     </style>
 </head>
@@ -16,7 +25,23 @@
         <h1>Formulario de inscrição</h1>
         <form id="formulario" action="controlerInscricao.php" method="POST">
             <p>Nome: <input type="text" name="lNome"></p>
+            <div class="mensErro">
+                <?php
+                    $mensagemDeErro = isset($_SESSION['mensagem-de-erro']) ? $_SESSION['mensagem-de-erro']: '';
+                    if(!empty($mensagemDeErro)){
+                        echo $mensagemDeErro;
+                    }
+                ?>
+            </div>
             <p>Idade: <input type="text" name="lIdade"> </p>
+            <div class="mensErro">
+                <?php
+                    $mensagemDeErroIdade = isset($_SESSION['mensagem-de-erro-idade']) ? $_SESSION['mensagem-de-erro-idade']:' ';
+                    if(!empty($mensagemDeErroIdade)){
+                        echo $mensagemDeErroIdade;
+                    }
+                ?>
+            </div>
             <p> <input type="submit" value="Inscrever-se"> </p>
         </form>
     </div>
